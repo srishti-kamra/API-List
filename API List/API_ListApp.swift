36f970484a2982309ac4aa1c6d9709ee32647249
Srishti Kamra <srishtikamra07@gmail.com>
2023-02-21 09:34:15 -0600

    Initial Commit

---
+++ API List/API_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ListApp.swift
+//  API List
+//
+//  Created by Srishti Kamra  on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct API_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
